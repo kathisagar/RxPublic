61e9f4b47ea31c71ab3185fc4821757d1c12d3ff
kdayasag <kdayasag@LH7U05CG51707G9.ms.ds.uhc.com>
2018-03-08 15:12:16 +0530

    First commit

---
+++ src/RxPublic/Regression/PharmacySearch.java
@@ -0,0 +1,28 @@
+package RxPublic.Regression;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+/*import org.testng.annotations.Test;*/
+import org.testng.annotations.Test;
+
+public class PharmacySearch  {
+    
+	public static void login(){
+		
+		System.setProperty("webdriver.ie.driver", "IEDriverServer.exe");
+		DesiredCapabilities cap=DesiredCapabilities.internetExplorer();
+		cap.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+		cap.setCapability("requirewindowfocus", true);
+		WebDriver driver=new InternetExplorerDriver(cap);
+		
+		driver.get("https://crx.benefits.catamaranrx.com/rxpublic/portal/memberMain?customer=DEMO");
+	}
+
+
+@Test
+
+public void logintest(){
+	login();
+}
+}
